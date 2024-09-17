10f95edbb3b6bd503e24034f519d03d3ea55060c
Tudor Anastasiu <tg.anastasiu@gmail.com>
2024-09-17 12:29:35 +0300

    Add more tests for `createCollection` function.

--- test/Listings.t.sol
+++ test/Listings.t.sol
@@ -8,16 +8,43 @@ import {ERC721Mock} from "./mocks/ERC721Mock.sol";
 
 contract ListingsTest is Test {
     Listings listings;
+
     ERC721Mock nft1;
+    ERC721Mock nft2;
+    ERC721Mock nft3;
+    ERC721Mock nft4;
 
     function setUp() public {
         listings = new Listings();
 
         nft1 = new ERC721Mock();
+        nft2 = new ERC721Mock();
+        nft3 = new ERC721Mock();
+        nft4 = new ERC721Mock();
+    }
+
+    function test_createCollection(uint256 tokenId) public {
+        vm.assume(tokenId != 0 && tokenId < type(uint256).max - 4);
+        uint256[] memory tokenIds = new uint256[](4);
+        address[] memory receivers = new address[](4);
+        for (uint256 i; i < 4; ++i) {
+            tokenIds[i] = tokenId + i;
+            receivers[i] = address(this);
+
+            nft1.mint(address(this), tokenId + i);
+        }
+
+        nft1.setApprovalForAll(address(listings), true);
+        
+        listings.createCollection("Mock Collection", "CMOCK", address(nft1), tokenIds, receivers);
     }
 
     function test_createCollectionNotEnoughNFTs(uint256 tokenId) public {
+        vm.assume(tokenId != 0);
+
         nft1.mint(address(this), tokenId);
+
+        nft1.setApprovalForAll(address(listings), true);
 
         uint256[] memory tokenIds = new uint256[](1);
         tokenIds[0] = tokenId;
