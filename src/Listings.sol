5c05e63946ebd355eaf0e9a99154e2b7f83a6005
Tudor Anastasiu <tg.anastasiu@gmail.com>
2024-09-24 15:19:55 +0300

    Inherit Ownable in Listings contract.

--- src/Listings.sol
+++ src/Listings.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.27;
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 import {IListings} from "./interfaces/IListings.sol";
 import {CollectionToken} from "./CollectionToken.sol";
 
-contract Listings is IListings, ReentrancyGuard, IERC721Receiver {
+contract Listings is IListings, ReentrancyGuard, IERC721Receiver, Ownable {
     event CollectionCreated(address collection, uint256[] tokenId, Listing listing);
 
     uint256 public constant BOOTSTRAP_NFTS = 4;
@@ -29,6 +30,8 @@ contract Listings is IListings, ReentrancyGuard, IERC721Receiver {
         require(collectionCreated[collection], CollectionNotExists());
         _;
     }
+
+    constructor() Ownable(msg.sender) {}
 
     function createCollection(CreateCollection calldata _createCollection) external override nonReentrant {
         require(!collectionCreated[_createCollection.collection], CollectionNotExists());
