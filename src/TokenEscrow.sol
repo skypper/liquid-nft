5c411d364c39869fcd701e6e9ec7df4c7e94dafc
Tudor Anastasiu <tg.anastasiu@gmail.com>
2024-09-26 14:09:56 +0300

    Emit events and add some code comments in TokenEscrow contract.

--- src/TokenEscrow.sol
+++ src/TokenEscrow.sol
@@ -3,18 +3,38 @@ pragma solidity ^0.8.27;
 
 import {CollectionToken} from "./CollectionToken.sol";
 
+/**
+ * Holds tokens in escrow for users to later withdraw
+ */
 abstract contract TokenEscrow {
+    // Emitted when a new deposit is made
+    event Deposited(address token, uint256 amount, address receiver);
+
+    // Emitted when a withdrawal is made
+    event Withdrawn(address caller, address token, uint256 amount, address recipient);
+
     error InsufficientBalance();
 
+    // Stores the deposited balances of the collection tokens for each user
     mapping(address user => mapping(address collection => uint256 amount)) public balances;
 
+    /**
+     * Internal function used to manage the accounting for collection tokens (mostly fee accrual)
+     */
     function _deposit(address token, uint256 amount, address receiver) internal {
         balances[receiver][token] += amount;
+
+        emit Deposited(token, amount, receiver);
     }
 
+    /**
+     * Enables a user to withdraw their allocated collection tokens
+     */
     function withdraw(address token, uint256 amount, address recipient) public {
         require(balances[msg.sender][token] >= amount, InsufficientBalance());
         balances[msg.sender][token] -= amount;
         CollectionToken(token).transfer(recipient, amount);
+
+        emit Withdrawn(msg.sender, token, amount, recipient);
     }
 }
