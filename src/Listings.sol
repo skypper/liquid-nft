407dcde3e6d47c5a906a28ff934aaf4be69ebbf5
Tudor Anastasiu <tg.anastasiu@gmail.com>
2024-10-02 18:07:26 +0300

    Downgrade to Solidity version 0.8.26 from 0.8.27 in order to work with Uniswap V4 codebase.

--- src/Listings.sol
+++ src/Listings.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.26;
 
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
