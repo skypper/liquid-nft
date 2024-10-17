2aa30f14396f4487e991b4e8fbfe601e41820bc6
Tudor Anastasiu <tg.anastasiu@gmail.com>
2024-10-17 15:45:33 +0300

    Remove `test_addLiquidity` test case from Listings test suite.

--- test/Listings.t.sol
+++ test/Listings.t.sol
@@ -200,20 +200,4 @@ contract ListingsTest is LiquidNFTTest {
         listings.transferOwnership(collection, tokenId, newOwner);
         assertTrue(listings.ownerOf(address(nft1), tokenId) == newOwner);
     }
-
-    function test_addLiquidity(uint256 tokenId) public {
-        uint256 tokenIdsCount = 4;
-        vm.assume(tokenId != 0 && tokenId < type(uint256).max - tokenIdsCount);
-
-        address user = makeAddr("user");
-
-        _createCollection(tokenId, tokenIdsCount, user);
-
-        address collectionToken = listings.getCollectionToken(address(nft1));
-        deal(address(nativeToken), address(uniswapV4Hook), 1 ether);
-        deal(collectionToken, address(uniswapV4Hook), 1 ether);
-
-        vm.prank(address(listings));
-        uniswapV4Hook.initializeCollection(address(nft1), 45765206694984738996961730 / 60 * 60, 1 ether, 1 ether);
-    }
 }
