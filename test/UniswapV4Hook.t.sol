defe8bafb0195a013a3d10bad7c698c713330e6b
Tudor Anastasiu <tg.anastasiu@gmail.com>
2024-10-17 17:30:49 +0300

    Add assertions in `test_addLiquidity` test case.

--- test/UniswapV4Hook.t.sol
+++ test/UniswapV4Hook.t.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.26;
 
 import {console} from "forge-std/Test.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
 
 import {LiquidNFTTest} from "./utils/LiquidNFTTest.sol";
 import {CollectionToken} from "../src/CollectionToken.sol";
 
 contract UniswapV4HookTest is LiquidNFTTest {
+    using PoolIdLibrary for PoolKey;
+
     function test_addLiquidity(uint256 tokenId) public {
         uint256 tokenIdsCount = 4;
         vm.assume(tokenId != 0 && tokenId < type(uint256).max - tokenIdsCount);
@@ -15,6 +19,16 @@ contract UniswapV4HookTest is LiquidNFTTest {
 
         _createCollection(tokenId, tokenIdsCount, user);
 
+        PoolKey memory poolKey = uniswapV4Hook.getPoolKey(address(nft1));
+        PoolId poolId = poolKey.toId();
+
+        // test that pool is uninitialized
+        (uint160 sqrtPriceX96,,,) = stateView.getSlot0(poolId);
+        assertTrue(sqrtPriceX96 == 0);
+
+        // test that there is no liquidity in the pool
+        assertTrue(stateView.getLiquidity(poolId) == 0);
+
         address collectionToken = listings.getCollectionToken(address(nft1));
         deal(address(nativeToken), address(listings), 1 ether);
         deal(collectionToken, address(listings), 1 ether);
@@ -22,8 +36,15 @@ contract UniswapV4HookTest is LiquidNFTTest {
         vm.startPrank(address(listings));
         nativeToken.approve(address(uniswapV4Hook), 1 ether);
         CollectionToken(collectionToken).approve(address(uniswapV4Hook), 1 ether);
-        
+
         uniswapV4Hook.initializeCollection(address(nft1), 45765206694984738996961730 / 60 * 60, 1 ether, 1 ether);
         vm.stopPrank();
+
+        // test that pool has been initialized
+        (sqrtPriceX96,,,) = stateView.getSlot0(poolId);
+        assertTrue(sqrtPriceX96 > 0);
+
+        // test that liquidity has been minted
+        assertTrue(stateView.getLiquidity(poolId) > 0);
     }
 }
